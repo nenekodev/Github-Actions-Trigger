ed85eb8a36a66aa9528c7a9ad568b392a6d496bc
Natsu Houraisan <hello@neneko.cn>
2022-03-11 17:16:01 +0800

    1.0.3: use format()

--- index.py
+++ index.py
@@ -3,16 +3,23 @@ import json
 import os
 
 def run():
-    payload = json.dumps({"ref": os.environ.get('GITHUB_BRANCH')})
-    header = {'Authorization': 'token ' + os.environ.get('GITHUB_TOKEN'),
+    GH_TOKEN = os.environ.get('GITHUB_TOKEN')
+    GH_USER = os.environ.get('GITHUB_USER')
+    GH_REPO = os.environ.get('GITHUB_REPO')
+    GH_BRANCH =  os.environ.get('GITHUB_BRANCH')
+    GH_ACTION =  os.environ.get('GITHUB_ACTION_FILE')
+
+    header = {'Authorization': f'token {GH_TOKEN}',
                 "Accept": "application/vnd.github.v3+json"}
+
+    # run workflow
     requests.post(
-        f'https://api.github.com/repos/' + os.environ.get('GITHUB_USER') + '/' + os.environ.get('GITHUB_REPO') + '/actions/workflows/' + os.environ.get('GITHUB_ACTION_FILE') + '/dispatches',
-        data = payload, headers = header)
+        f'https://api.github.com/repos/{GH_USER}/{GH_REPO}/actions/workflows/{GH_ACTION}/dispatches',
+        data = json.dumps({"ref": GH_BRANCH }), headers = header)
     
     # delete workflow runs
     runsListJson = requests.get(
-        f'https://api.github.com/repos/' + os.environ.get('GITHUB_USER') + '/' + os.environ.get('GITHUB_REPO') + '/actions/runs',
+        f'https://api.github.com/repos/{GH_USER}/{GH_REPO}/actions/runs',
         headers = header)
     runsList = json.loads(runsListJson.content)
     workflowList = runsList['workflow_runs']
@@ -20,11 +27,11 @@ def run():
     for x in range(len(workflowList)):
         if workflowList[x]['conclusion'] == 'success':
             completedNum = workflowList[x]['id']
-            deleteUrl = 'https://api.github.com/repos/' + os.environ.get('GITHUB_USER') + '/' + os.environ.get('GITHUB_REPO') + '/actions/runs/' + str(completedNum)
+            deleteUrl = f'https://api.github.com/repos/{GH_USER}/{GH_REPO}/actions/runs/{str(completedNum)}'
             requests.delete(
                 deleteUrl,
                 headers = header)
 
-# SCF entrance
+# SCF handler
 def main_handler(event, context):
     return run()
